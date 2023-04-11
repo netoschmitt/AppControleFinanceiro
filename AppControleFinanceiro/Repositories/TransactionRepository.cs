e3fcedda8b251478495cafc121bff69bfbf6c0cf
netoschmitt <agneloschmittneto@gmail.com>
2023-04-11 14:32:28 -0300

    Adicionar arquivos de projeto.

---
+++ AppControleFinanceiro/Repositories/TransactionRepository.cs
@@ -0,0 +1,44 @@
+﻿using AppControleFinanceiro.Models;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControleFinanceiro.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly LiteDatabase _database;
+        private readonly string collectionName = "transactions";
+        public TransactionRepository(LiteDatabase database)
+        {
+            _database = database;
+        }
+        public List<Transaction> GetAll()
+        {
+            return _database
+                .GetCollection<Transaction>(collectionName)
+                .Query()
+                .OrderByDescending(a=>a.Date)
+                .ToList();
+        }
+        public void Add(Transaction transaction)
+        {
+            var col = _database.GetCollection<Transaction>(collectionName);
+            col.Insert(transaction);
+            col.EnsureIndex(a => a.Date);
+        }
+        public void Update(Transaction transaction)
+        {
+            var col = _database.GetCollection<Transaction>(collectionName);
+            col.Update(transaction);
+        }
+        public void Delete(Transaction transaction)
+        {
+            var col = _database.GetCollection<Transaction>(collectionName);
+            col.Delete(transaction.Id);
+        }
+    }
+}
